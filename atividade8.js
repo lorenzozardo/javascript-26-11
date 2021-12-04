803ebb70b0c44467a24f8bbcd662fdf068d30505
Lorenzo Zardo <92864867+lorenzozardo@users.noreply.github.com>
2021-12-04 14:27:35 -0300

    Add files via upload

---
+++ atividade8.js
@@ -0,0 +1,9 @@
+var num1 = prompt ('digite um número');
+var num2 = prompt('digite um número');
+
+soma = num1 + num2;
+
+soma2 = parseInt(num1) + parseInt(num2);
+
+alert(soma2);
+document.write(soma);
